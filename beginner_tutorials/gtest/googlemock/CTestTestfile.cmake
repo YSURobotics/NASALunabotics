f3d5353fab388115da6b94796abe7d5e2a37ce92
YSURobotics <ryarwood99@gmail.com>
2021-03-26 11:58:40 -0400

    removed beginner tutorials

--- beginner_tutorials/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/rjyarwood/catkin_ws/src/beginner_tutorials/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
