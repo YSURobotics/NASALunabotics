e81867c42fb32d56f743d624031a290f5616b5c7
RJ Yarwood <rjyarwood99@gmail.com>
2021-04-21 17:45:45 -0700

    style

--- controller/src/view_rover_status.cpp
+++ controller/src/view_rover_status.cpp
@@ -16,46 +16,45 @@ void Rover::messageCallback(const controller::Rover::ConstPtr& msg) {
     timestamp = ctime(&now);
     timestamp = timestamp.substr(0, timestamp.length() - 1);
 
-    std::cout << timestamp << " Rover Status: \n";
+    std::cout << timestamp << " Rover Status: \n\n";
 
-    std::string test = std::string(BOLD(UNDL(FGRN("High")))) + (FGRY("Low\n"));
+        boost_string      = ((msg->auger & BIT4) ? std::string(BOLD(UNDL(FGRN("High")))) + "  " + (FGRY("Low\n")) : std::string(FGRY("High  ")) + BOLD(UNDL(FYEL("Low\n"))));
 
-        boost_string      = ((msg->auger & BIT4) ? std::string(BOLD(UNDL(FGRN("High")))) + (FGRY("Low\n")) : std::string(FGRY("High")) + BOLD(UNDL(FYEL("Low\n"))));
+        rail_right_string = ((msg->rail & BIT0) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + "  " + FGRY("Disengaged\n"): std::string(FGRY("Engaged  ")) + BOLD(UNDL(FRED("Disengaged\n"))));
+        rail_left_string  = ((msg->rail & BIT1) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + "  " + FGRY("Disengaged\n"): std::string(FGRY("Engaged  ")) + BOLD(UNDL(FRED("Disengaged\n"))));
+        rail_up_string    = ((msg->rail & BIT2) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + "  " + FGRY("Disengaged\n"): std::string(FGRY("Engaged  ")) + BOLD(UNDL(FRED("Disengaged\n"))));
+        rail_down_string  = ((msg->rail & BIT3) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + "  " + FGRY("Disengaged\n"): std::string(FGRY("Engaged  ")) + BOLD(UNDL(FRED("Disengaged\n"))));
 
+        auger_right_string = ((msg->auger & BIT0) ? std::string(BOLD(UNDL(FGRN("Drilling")))) + "  " + FGRY("Undrilling  Stopped\n") : (msg->auger & BIT1) ? std::string(FGRY("Drilling")) + "  " + BOLD(UNDL(FYEL("Undrilling"))) + "  "  + FGRY("Stopped\n") : std::string(FGRY("Drilling  ")) + FGRY("Undrilling  ") + FRED("Stopped\n"));
+        auger_left_string = ((msg->auger & BIT2) ? std::string(BOLD(UNDL(FGRN("Drilling")))) + "  " + FGRY("Undrilling  Stopped\n") : (msg->auger & BIT3) ? std::string(FGRY("Drilling")) + "  " + BOLD(UNDL(FYEL("Undrilling"))) + "  " + FGRY("Stopped\n") : std::string(FGRY("Drilling  ")) + FGRY("Undrilling  ") + FRED("Stopped\n"));
 
-        rail_right_string = ((msg->rail & BIT0) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + FGRY("Disengaged\n"): std::string(FGRY("Engaged")) + BOLD(UNDL(FRED("Disengaged\n"))));
-        rail_left_string  = ((msg->rail & BIT1) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + FGRY("Disengaged\n"): std::string(FGRY("Engaged")) + BOLD(UNDL(FRED("Disengaged\n"))));
-        rail_up_string    = ((msg->rail & BIT2) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + FGRY("Disengaged\n"): std::string(FGRY("Engaged")) + BOLD(UNDL(FRED("Disengaged\n"))));
-        rail_down_string  = ((msg->rail & BIT3) ? std::string(BOLD(UNDL(FGRN("Engaged")))) + FGRY("Disengaged\n"): std::string(FGRY("Engaged")) + BOLD(UNDL(FRED("Disengaged\n"))));
-
-        auger_right_string = ((msg->auger & BIT0) ? std::string(BOLD(UNDL(FGRN("Drilling")))) + FGRY("Undrilling Stopped\n") : (msg->auger & BIT1) ? std::string(FGRY("Drilling")) + BOLD(UNDL(FYEL("Undrilling")))  + FGRY("Stopped\n") : std::string(FGRY("Drilling")) + FGRY("Undrilling") + FRED("Stopped\n"));
-        auger_left_string = ((msg->auger & BIT2) ? std::string(BOLD(UNDL(FGRN("Drilling")))) + FGRY("Undrilling Stopped\n") : (msg->auger & BIT3) ? std::string(FGRY("Drilling")) + BOLD(UNDL(FYEL("Undrilling")))  + FGRY("Stopped\n") : std::string(FGRY("Drilling")) + FGRY("Undrilling") + FRED("Stopped\n"));
-
-        dump_string = ((msg->dump & BIT1) ? std::string(BOLD(UNDL(FGRN("Dumping")))) + FGRY("Lowering Stopped\n"): (msg->dump & BIT0) ? std::string(FGRY("Dumping")) + BOLD(UNDL(FYEL("Lowering"))) + FGRY("Stopped\n") : std::string(FGRY("Dumping")) + FGRY("Lowering") + FRED("Stopped\n"));
+        dump_string = ((msg->dump & BIT1) ? std::string(BOLD(UNDL(FGRN("Dumping")))) + "  " + FGRY("Lowering  Stopped\n"): (msg->dump & BIT0) ? std::string(FGRY("Dumping  ")) + BOLD(UNDL(FYEL("Lowering"))) + "  " + FGRY("Stopped\n") : std::string(FGRY("Dumping  ")) + FGRY("Lowering  ") + FRED("Stopped\n"));
 
         std::cout << "\tBoost: " << boost_string;
 
+        std::cout << "\n";
+
         std::cout << "\tDrivetrain Status: \n";
-            std::cout << "\t\tLeft Velocity: " << msg->left_vel << "\n" << 
-                         "\t\tRight Velocity: " << msg->right_vel << "\n";
+            std::cout << "\t\tLeft Velocity:  " << (msg->left_vel - 11) << "\n" << 
+                         "\t\tRight Velocity: " << (msg->right_vel - 11) << "\n";
 
         std::cout << "\n";
 
         std::cout << "\tRail Status: \n";
-            std::cout << "\t\tRight Drive: "   << rail_right_string;
-            std::cout << "\t\tLeft Drive: "    << rail_left_string; 
-            std::cout << "\t\tRotating Up: "   << rail_up_string; 
+            std::cout << "\t\tRight Drive:   "   << rail_right_string;
+            std::cout << "\t\tLeft  Drive:   "    << rail_left_string; 
+            std::cout << "\t\tRotating Up:   "   << rail_up_string; 
             std::cout << "\t\tRotating Down: " << rail_down_string; 
 
         std::cout << "\n";
 
         std::cout << "\tAuger Status: \n";
             std::cout << "\t\tRight Auger: " << auger_right_string;
-            std::cout << "\t\tLeft Auger: "  << auger_left_string;
-           
+            std::cout << "\t\tLeft  Auger: "  << auger_left_string;
+            
         std::cout << "\n";
 
-        std::cout << "\tDump Status: \n";
+            std::cout << "\tDump Status: \n";
             std::cout << "\t\tDump Bin: " << dump_string;
 
 
